94f979a5846d0116b6114042e239aac97f230cbd
Denis Balabanov <balabanov.ds@gmail.com>
2020-05-23 07:23:41 +0300

    create migrations

--- migrations/20200520114255_users.down.sql
+++ migrations/20200520114255_users.down.sql
@@ -1,1 +1,4 @@
+drop table if exists profiles_ru;
+drop table if exists profiles;
+drop table if exists roles;
 drop table if exists users;
--- migrations/20200520114255_users.up.sql
+++ migrations/20200520114255_users.up.sql
@@ -7,3 +7,37 @@ create table users
     active boolean default true
 );
 
+create table roles
+(
+    id smallint primary key not null,
+    role varchar(32)
+);
+
+insert into roles values
+(1, 'engineer'),
+(2, 'manager'),
+(99, 'admin');
+
+create table profiles
+(
+    id serial primary key,
+    user_id int references users unique,
+    first_name varchar (255),
+    last_name varchar(255),
+    position varchar (255),
+    company_id int,
+    z_code varchar(10),
+    manager_id int references profiles,
+    role_id smallint references roles,
+    date_modified timestamp
+);
+
+create table profiles_ru
+(
+    id serial primary key,
+    profile_id int references profiles,
+    first_name varchar (255),
+    last_name varchar(255),
+    position varchar (255),
+    date_modified timestamp
+);
